4b9e161e5b115e19cae132258509842f96eadd63
De Ville Weppenaar <dweppenaar@gmail.com>
2015-05-16 19:56:32 +0200

    Added 'Event' section code.

--- leaderboard.js
+++ leaderboard.js
@@ -3,12 +3,23 @@ PlayerList = new Mongo.Collection('players');
 if (Meteor.isClient) {
 
   Template.leaderboard.helpers({
+    
     'player': function() {
       return PlayerList.find();
     },
+    
     'playerCount': function() {
       return PlayerList.find().count();
     }
+
+  });
+
+  Template.leaderboard.events({
+    
+    'click .player': function () {
+      console.log("You clicked an li element");
+    }
+
   });
 
 }
